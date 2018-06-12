6d7dcb5735a455cea7e6357c802ec022c7cbcdb1
Arpan Roy <Arpan.Roy@mea.ey.net>
2018-06-12 12:49:01 +0530

    lombok demo

---
+++ src/main/java/com/example/webfluxdemo/model/Tweet.java
@@ -0,0 +1,35 @@
+package com.example.webfluxdemo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "tweets")
+public class Tweet {
+    @Id
+    private String id;
+
+    @NotBlank
+    @Size(max = 140)
+    private String text;
+
+    @NotNull
+    private Date createdAt = new Date();
+
+    public Tweet(String text) {
+        this.id = id;
+        this.text = text;
+    }
+}
